10689c5e02593c936eed012d0dedade7ee94c77e
kos234 <kostyaperfiliev94@gmail.com>
2020-04-24 20:31:48 +0800

    s

--- web/callback.php
+++ web/callback.php
@@ -1,11 +1,14 @@
 <?php
 $code = $_GET['code'];
+var_dump($_POST);
 error_log(code);
 if( $curl = curl_init() ) {
+    curl_setopt($curl, CURLOPT_USERPWD, "dde6a297cdc345059eda98c69ba722c0:ce45e9cbc7da47019b6540f9abe00a68");
     curl_setopt($curl, CURLOPT_URL, 'https://accounts.spotify.com/api/token');
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($curl, CURLOPT_POST, true);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, "grant_type=authorization_code&code=" . code . + "&redirect_uri=https://music-statuc-by-kos.herokuapp.com/callback");
+    curl_setopt($curl, CURLSSLOPT_NO_REVOKE, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, "grant_type=authorization_code&code=" . code . +"&redirect_uri=https://music-statuc-by-kos.herokuapp.com/callback");
     $out = curl_exec($curl);
     echo $out;
     curl_close($curl);
