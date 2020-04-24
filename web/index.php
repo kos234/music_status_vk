151e8703f0ebcd0bd0c5657180f6dfcdba24e9ba
kos234 <kostyaperfiliev94@gmail.com>
2020-04-24 22:19:04 +0800

    s

--- web/index.php
+++ web/index.php
@@ -10,7 +10,8 @@
 <script src="https://sdk.scdn.co/spotify-player.js"></script>
 <script>
     window.onSpotifyWebPlaybackSDKReady = () => {
-        const token = 'BQC3d4N48WDKbu3bp97yacBtamrRgrrluW73C2wmLpzLVX_6zwnBEYlGC9C--2gcAtDxhSlV64G19CuTfqlt5Gkt_ksRMF5_daudm89UqmQXiFb6tFCdrKkaRjr9N5JFfQDCXSM8JGDBbMsi-NLsmxZEZFBswZlAI8EB6iG_4U5dZzIO9-s';
+        const token = "BQAirMhTosxQJ78wmBI_a6Nwdbdmzty6y3uD6i-KDiJL31bmOQD4Ox9xRR7q0iCoMC1HBu8PO4KKnT-weI6TMpSDLHOPaTVgvMT7PvIxClepuCnHpZ9DK7uQcpuoRP7Spk2_iAeg60uzz-HQy8Hvn46JlS80uWFi25Xg-B0tuMyoogY\n" +
+            "9F1jaouHHEim-LX5z6panyHWFXUvlNTANSrVQmsCmVBsb0USf-FKvIUwA54GqY7ewAN29Y9VDu0Cu2e-Ayf666gPkXHbR_iK_ri_q0Rp8PDbkmjxDtAthV1wjlBY";
         const player = new Spotify.Player({
             name: 'Web Playback SDK Quick Start Player',
             getOAuthToken: cb => { cb(token); }
