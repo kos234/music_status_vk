b1d316395baa1a285dc465578f0305540db09f0f
kos234 <kostyaperfiliev94@gmail.com>
2020-04-24 19:25:12 +0800

    s

--- web/index.php
+++ web/index.php
@@ -1,25 +1,27 @@
-<?php
+<html>
+<head>
+    <title>Тест</title>
+</head>
+<body>
+<script src="https://sdk.scdn.co/spotify-player.js"></script>
+<script>
+    window.onSpotifyWebPlaybackSDKReady = () => {
+        var player = new Spotify.Player({
+            name: 'kos',
+            getOAuthToken: callback => {
 
-require('../vendor/autoload.php');
 
-$app = new Silex\Application();
-$app['debug'] = true;
+                callback('BQBIFsbMgOgwNdEu1nO-e-7q8iQ6msM-RfaREdvweJO0Y_iS_ousfj3fHIPya7yADnQFmEpwU8Fujet2_rzc4DfBnOPX5Mb6acZLc4IpqzhNeGGV7Zlc2aqLh12AaDPcMfqqv6DwVJN5i5bG0KEQXk18J9VrVegTdsJjXS2Gjebm3WP0ZSP7GnUretvYclnaLRlOQj9NB_EQgD4ZnQGg__IaB5Ma79mmQ39SYgvEZSWOhFCFdYAMCXFSLwLXeJTWRHXj4CNPRvXHQKFEjmQVA2vKbcqLBeFHp6aIvkcN');
+            },
+            volume: 0.5
+        });
 
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
-
-$app->run();
+        player.connect().then(success => {
+            if (success) {
+                console.log('The Web Playback SDK successfully connected to Spotify!');
+            }
+        })
+    };
+</script>
+</body>
+</html>
