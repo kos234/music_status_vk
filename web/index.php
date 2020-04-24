faaf938a897d9974796518070653ecccab03c6f4
kos234 <kostyaperfiliev94@gmail.com>
2020-04-24 23:38:56 +0800

    s

--- web/index.php
+++ web/index.php
@@ -17,21 +17,21 @@
         });
 
         // Error handling
-        player.addListener('initialization_error', ({ message }) => { console.error(message); });
-        player.addListener('authentication_error', ({ message }) => { console.error(message); });
-        player.addListener('account_error', ({ message }) => { console.error(message); });
-        player.addListener('playback_error', ({ message }) => { console.error(message); });
+        player.addEventListener('initialization_error', ({ message }) => { console.error(message); });
+        player.addEventListener('authentication_error', ({ message }) => { console.error(message); });
+        player.addEventListener('account_error', ({ message }) => { console.error(message); });
+        player.addEventListener('playback_error', ({ message }) => { console.error(message); });
 
         // Playback status updates
-        player.addListener('player_state_changed', state => { console.log(state); });
+        player.addEventListener('player_state_changed', state => { console.log(state); });
 
         // Ready
-        player.addListener('ready', ({ device_id }) => {
+        player.addEventListener('ready', ({ device_id }) => {
             console.log('Ready with Device ID', device_id);
         });
 
         // Not Ready
-        player.addListener('not_ready', ({ device_id }) => {
+        player.addEventListener('not_ready', ({ device_id }) => {
             console.log('Device ID has gone offline', device_id);
         });
 
