05a0fe70f60fbb5241e47946ba38735dd8a2eb9f
kos234 <kostyaperfiliev94@gmail.com>
2020-04-24 20:55:29 +0800

    s

--- web/index.php
+++ web/index.php
@@ -5,7 +5,7 @@
 <body>
 <script src="https://sdk.scdn.co/spotify-player.js"></script>
 <script>
-    window.onSpotifyWebPlaybackSDKReady = () => {
+    window.onSpotifyWebPlaybackSDKReady = listener => {
         var player = new Spotify.Player({
             name: 'kos',
             getOAuthToken: callback => {
@@ -20,14 +20,13 @@
         player.connect().then(success => {
             if (success) {
                 console.log('The Web Playback SDK successfully connected to Spotify!');
-
-                player.addListener('player_state_changed', ({position, duration, track_window: { current_track }}) => {
-                    console.log('Currently Playing', current_track);
-                    console.log('Position in Song', position);
-                    console.log('Duration of Song', duration);
-                });
             }
-        })
+        });
+        player.addListener('player_state_changed', ({position, duration, track_window: {current_track}}) => {
+            console.log('Currently Playing', current_track);
+            console.log('Position in Song', position);
+            console.log('Duration of Song', duration);
+        });
     };
 </script>
 </body>
