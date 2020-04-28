3455b7fb84f503d7c385f89c4e387f20b6f6206f
kos234 <kostyaperfiliev94@gmail.com>
2020-04-28 23:52:46 +0800

    s

--- web/index.php
+++ web/index.php
@@ -1,54 +1,20 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Spotify Web Playback SDK Quick Start Tutorial</title>
-</head>
-<body>
-<h1>Spotify Web Playback SDK Quick Start Tutorial</h1>
-<h2>Open your console log: <code>View > Developer > JavaScript Console</code></h2>
+<?php
+//ini_set('max_execution_time', 31536000); // Чтобы наш скрипт выполнялся
 
-<script src="https://sdk.scdn.co/spotify-player.js"></script>
-<script>
-    window.onSpotifyWebPlaybackSDKReady =  () => {
-        const token = "BQBS5hI1_E2ol3UHaGUSFYctduiKx7Mjvq9eQN6C25efCE1BLMf8e1vn7zWMfS_Q7yzSQu_X6yIbK6-hTADh7vqFWtsUhkOGMBezI2Kna51pYvAQose-o6K7VXitXrlsx6kr-bzJKCpGW6pnzKZKNR2ZeCuc2aFMjN5G0rvz1NOgiC7x5Zhzu9Qb7vuOEEH67qTJh6iPvHWB5f20rInmTrNxgiZMDR5Ws3E5Uyr04eGqhK76nhrtJi-rV2NgXlaRJ0p2hM1xZYVLxddDwhw1pw-C2XF_WGO312aj0TmXbac";
-        const player = new Spotify.Player({
-            name: 'Web Playback SDK Quick Start Player',
-            volume: 1.0,
-            getOAuthToken: cb => {
-                cb(token);
-            }
-        });
+$urlDB=parse_url(getenv("CLEARDB_DATABASE_URL")); //Подключаемся к бд
 
-        // Error handling
-        player.addListener('initialization_error', ({message}) => {
-            console.error(message);
-        });
-        player.addListener('authentication_error', ({message}) => {
-            console.error(message);
-        });
-        player.addListener('account_error', ({message}) => {
-            console.error(message);
-        });
-        player.addListener('playback_error', ({message}) => {
-            console.error(message);
-        });
+$server = $urlDB["host"];
+$username = $urlDB["user"];
+$password = $urlDB["pass"];
+$db = substr($urlDB["path"],1)
 
+echo $server.' <- сервер '.$username.' <- имя пользователя '.$password.' <- пароль '.$db.' <- база данных'; //Если нужно узнать данные бд
 
-        // Ready
-        player.addListener('ready', ({device_id}) => {
-            console.log('Ready with Device ID', device_id);
+$mysqli = new mysqli($server, $username, $password,$db); //Подключаемся
 
-            player.addListener('player_state_changed', state => { console.log(state); });
-        });
+  if ($mysqli->connect_error) {//проверка подключились ли мы
+      die('Ошибка подключения (' . $mysqli->connect_errno . ') '. $mysqli->connect_error); //если нет выводим ошибку и выходим из кода
+  } else {
 
-        // Not Ready
-        player.addListener('not_ready', ({device_id}) => {
-            console.log('Device ID has gone offline', device_id);
-        });
-
-        // Connect to the player!
-        player.connect();
-    };
-</script>
-</body>
-</html>
+  }
+    ?>
