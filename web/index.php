6e6bc4b2c3648186af2e55264c97ec9619d26668
kos234 <kostyaperfiliev94@gmail.com>
2020-04-28 23:05:49 +0800

    s

--- web/index.php
+++ web/index.php
@@ -9,7 +9,7 @@
 
 <script src="https://sdk.scdn.co/spotify-player.js"></script>
 <script>
-    window.onSpotifyWebPlaybackSDKReady = async () => {
+    window.onSpotifyWebPlaybackSDKReady =  () => {
         const token = "BQBS5hI1_E2ol3UHaGUSFYctduiKx7Mjvq9eQN6C25efCE1BLMf8e1vn7zWMfS_Q7yzSQu_X6yIbK6-hTADh7vqFWtsUhkOGMBezI2Kna51pYvAQose-o6K7VXitXrlsx6kr-bzJKCpGW6pnzKZKNR2ZeCuc2aFMjN5G0rvz1NOgiC7x5Zhzu9Qb7vuOEEH67qTJh6iPvHWB5f20rInmTrNxgiZMDR5Ws3E5Uyr04eGqhK76nhrtJi-rV2NgXlaRJ0p2hM1xZYVLxddDwhw1pw-C2XF_WGO312aj0TmXbac";
         const player = new Spotify.Player({
             name: 'Web Playback SDK Quick Start Player',
@@ -38,7 +38,7 @@
         player.addListener('ready', ({device_id}) => {
             console.log('Ready with Device ID', device_id);
 
-            let state = await player.getCurrentState();
+            let state = player.getCurrentState();
             if (state == null) {
                 console.error("Playback isn't on this device yet");
             } else {
