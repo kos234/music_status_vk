7da6c3b929dd995bbe0fa93f0a30c0384fbaaae5
kos234 <kostyaperfiliev94@gmail.com>
2020-04-29 00:26:52 +0800

    s

--- web/index.php
+++ web/index.php
@@ -30,7 +30,7 @@ ENGINE = InnoDB;"); //Создаем таблицу в бд
           $operationId = $mysqli->query("SELECT `operationId` FROM `data` ");
           if($operationId == 0) continue;
           elseif ($operationId == 1){
-              $statusJSON = json_decode(file_get_contents("https://api.vk.com/method/status.get?access_token=" . $token . "&user_id=". $user_id ."&v=". $v));
+              $statusJSON = json_decode(file_get_contents("https://api.vk.com/method/status.get?access_token=" . $tokenVk . "&user_id=". $user_id ."&v=". $versionAPI));
               $status = $statusJSON->response[0]->text;
               $mysqli->query("UPDATE `data` set `lastStatus` = $status , `operationID` = 2");
           }elseif ($operationId == 2){
