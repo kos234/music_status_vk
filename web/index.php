815084c42bd847d36d26f5fd4c6604fe8ad285a3
kos234 <kostyaperfiliev94@gmail.com>
2020-04-30 00:40:31 +0800

    s

--- web/index.php
+++ web/index.php
@@ -1,9 +1,10 @@
 <?php
 ini_set('max_execution_time', 900); // Чтобы наш скрипт выполнялся
 
-/*$urlDB=parse_url(getenv("CLEARDB_DATABASE_URL")); //Подключаемся к бд
+$urlDB=parse_url(getenv("CLEARDB_DATABASE_URL")); //Подключаемся к бд
 
 $tokenVk = "2816968ae753fdf5d35ed88fa6b396a219b0712f28e38d169cd0a04e8851c57eae6a29077cdfa8493a241";
+$tokenSpotify = "BQC8i9bw5KEMa4xzhuF36sCIMLs4eaa6qWBu1BFDxmccEaQfuWRqzx4j26nJjNBwcnLCz8nF9_CwLYAIgnSs8I5Pk7Q8XTNfsI1kUdWaCr_tgUkq8eHPOuum1aN_jTpeDhqjKxTMr9yaLufWFDvyqPFIhPGDT0np7q3xV8R8YViJnVSznf6qiT-EzCjSU3iRnHDy9r3nCIByMW-zJ1dhtv2Brf4m1Nia5v2RoJu-AycECQv8A0RAygdTCAZKU18fag9BMinz9IvKR99jEJJsOKrOu2jS1L3yH3qm6PHaE3E";
 $versionAPI = "5.103";
 $user_id = "388061716";
 
@@ -41,17 +42,26 @@ ENGINE = InnoDB;"); //Создаем таблицу в бд
           elseif ($operationId['operationId'] == "start"){
               $statusJSON = json_decode(file_get_contents("https://api.vk.com/method/status.get?access_token=" . $tokenVk . "&user_id=". $user_id ."&v=". $versionAPI));
               $status = $statusJSON->response->text;
-              $error = $mysqli->query("UPDATE dataSettings set lastStatus =  '$status' , operationID = 'on'");
-              if($error) {
-                  error_log('Прокатило;');
-              } else {
-                  error_log('Хьюстон! У нас проблемы!'.mysqli_error($error));
-
-              }
+              $mysqli->query("UPDATE dataSettings set lastStatus =  '$status' , operationID = 'on'");
               error_log($status);
           } elseif ($operationId['operationId'] == "on"){
+            $trackJSON = json_decode(file_get_contents("https://api.spotify.com/v1/me/player/currently-playing?access_token=" . $tokenSpotify));
+            $count = 0;
+            $artists = "";
+            $album = "";
+            while (isset($trackJSON->item->artists[$count]->name)){
+               $artists = $artists + $trackJSON->item->artists[$count]->name . ", ";
+            }
 
+            if(isset($trackJSON->item->album))
+                if($trackJSON->item->album->type == "album")
+                    $album = " , Альбом: " . $trackJSON->item->album->name;
 
+            $status = "Слушает: " . substr($artists, -2) . " - " . $trackJSON->item->name . $album;
+
+            error_log("Получилось -> ." .$status);
+
+            sleep(60);
 
           }elseif ($operationId['operationId'] == "finish"){
 
@@ -60,5 +70,5 @@ ENGINE = InnoDB;"); //Создаем таблицу в бд
           }
       }
       error_log("End");
-  } */
+  }
     ?>
