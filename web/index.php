f7f9c180312c43221fdbcf16d1d0397169c9467c
kos234 <kostyaperfiliev94@gmail.com>
2020-04-29 23:17:51 +0800

    s

--- web/index.php
+++ web/index.php
@@ -1,7 +1,19 @@
 <?php
 ini_set('max_execution_time', 900); // Чтобы наш скрипт выполнялся
 
-$urlDB=parse_url(getenv("CLEARDB_DATABASE_URL")); //Подключаемся к бд
+use GuzzleHttp\Client;
+
+$client = new GuzzleHttp\Client();
+$res = $client->post('https://api.spotify.com/v1/me', [
+    'headers' => [
+        'Authorization' =>  ['Bearer ' . "BQAKmny4Ul7HS8I4TOueVYuHgSU8OUzCw9B_h2ZPs8STWaLLnvsYzjChPp8T0gfyD5bgeKK3Gzq1zhQYmzXrXIONZgv6_NgWDzgtG8yF7550NIqqsyLmWm5-aOwUs5vaXz4NGnrTNf5SRvQsHnhfrzhGbPWeBUOgrj7zWqjRp4Adbi-kqCXkFR5ReHbViozwRF711HlmDResIZXH9QeuQdMtzMgxfiOrfRUNexZiCA11e6_5hbTCxnfNwg5wOdPP3iD0G7A87kCZ485P2edimpVnQVzY4CI1"],
+    ]
+]);
+
+echo $res->getStatusCode(); // 200
+echo $res->getBody();
+
+/*$urlDB=parse_url(getenv("CLEARDB_DATABASE_URL")); //Подключаемся к бд
 
 $tokenVk = "2816968ae753fdf5d35ed88fa6b396a219b0712f28e38d169cd0a04e8851c57eae6a29077cdfa8493a241";
 $versionAPI = "5.103";
@@ -50,9 +62,8 @@ ENGINE = InnoDB;"); //Создаем таблицу в бд
               }
               error_log($status);
           } elseif ($operationId['operationId'] == "on"){
-              error_log("22222");
-              break;
-            break;
+
+
 
           }elseif ($operationId['operationId'] == "finish"){
 
@@ -61,5 +72,5 @@ ENGINE = InnoDB;"); //Создаем таблицу в бд
           }
       }
       error_log("End");
-  }
+  } */
     ?>
