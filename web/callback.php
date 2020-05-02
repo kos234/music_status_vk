8e5ebb053d8ffff0398da4a5a9224c0020c4a622
kos234 <kostyaperfiliev94@gmail.com>
2020-05-03 03:35:41 +0800

    s

--- web/callback.php
+++ web/callback.php
@@ -2,7 +2,11 @@
 
 if(isset($_GET['code'])){
     $token = json_decode(file_get_contents("https://oauth.vk.com/access_token?client_id=7445793&client_secret=Wo2hagteHrHp6VxjHMcK&redirect_uri=https://music-statuc-by-kos.herokuapp.com/callback&code=" . $_GET['code']));
-    print_r($token);
+    if (isset($token->access_token)){
+        error_log($token->access_token);
+    }
+}elseif (isset($_GET['access_token'])){
+    error_log($_GET['access_token']);
 }
 
 ?>
