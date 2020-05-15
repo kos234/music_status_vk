70cc2398b1cc332008c9891954bd3e50e8ad73b1
kos234 <kostyaperfiliev94@gmail.com>
2020-05-16 01:21:19 +0800

    bug fix

--- web/index.php
+++ web/index.php
@@ -194,8 +194,10 @@ $app->post('/bot', function () use ($app) {
                         $sec_padej = " секунды ";
                     else $sec_padej = " ворнинг!" . endNumber($sec) . " ";
 
-                    error_log("one" . endNumber($sec) >= 2);
-                    error_log("two" . endNumber($sec) <= 4);
+                    error_log("one");
+                    error_log(endNumber($sec) >= 2);
+                    error_log("two");
+                    error_log(endNumber($sec) <= 4);
 
                     $request_params['message'] = "Последний ответ был " . $sec . $sec_padej . "назад." . $type;
 
